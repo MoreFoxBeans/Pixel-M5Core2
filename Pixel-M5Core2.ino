51c7074c67145fee2a166a4c00aaaa8c2747a037
Seth H <83931313+MoreFoxBeans@users.noreply.github.com>
2022-11-27 21:07:48 -0700

    Change font, make canvas draggable, other stuff

--- Pixel-M5Core2.ino
+++ Pixel-M5Core2.ino
@@ -1,6 +1,9 @@
 #include <SD.h>
 #include <M5Unified.h>
 
+// https://rop.nl/truetype2gfx/
+#include "Pixel128pt7b.h"
+
 #define CANVAS_WIDTH    64
 #define CANVAS_HEIGHT   32
 
@@ -9,19 +12,28 @@
 #define BORDER_COLOR        0x632C
 #define SEPERATOR_COLOR     0xCE79
 
-M5Canvas gui(&M5.Display);
-M5Canvas canvas(&M5.Display);
+M5Canvas screen(&M5.Display);
+M5Canvas gui(&screen);
+M5Canvas canvas(&screen);
 
-uint16_t viewX = 0;
-uint16_t viewY = 0;
-uint16_t viewZ = 4;
+uint8_t pnum;
+uint8_t num;
+int16_t otx;
+int16_t oty;
+int16_t oviewX;
+int16_t oviewY;
+
+int16_t viewX = 0;
+int16_t viewY = 0;
+uint8_t viewZ = 4;
 
 uint32_t frames = 0;
 
-void drawScreen(bool whole=false, bool clock=false);
+void drawScreen(bool whole, bool drawClock, bool drawCanvas);
 
 void setup(void) {
   auto cfg = M5.config();
+  cfg.serial_baudrate = 115200;
   cfg.clear_display  = true;
   cfg.output_power   = false;
   cfg.internal_imu   = true;
@@ -37,73 +49,109 @@ void setup(void) {
   
   M5.Display.setBrightness(32);
   M5.Display.begin();
-  M5.Display.loadFont("font", SD);
+
+  screen.createSprite(320, 240);
+  screen.fillSprite(BACKGROUND_COLOR);
+  screen.setTextSize(1);
+  screen.setFont(&Pixel128pt7b);
+  screen.setTextWrap(false, false);
 
   gui.createSprite(320, 240);
   gui.fillSprite(TFT_TRANSPARENT);
-  gui.fillRect(0, 202, 320, 2, BORDER_COLOR);
-  gui.fillRect(0, 204, 320, 36, GUI_COLOR);
-  gui.drawFastVLine(41, 212, 20, SEPERATOR_COLOR);
-  gui.drawFastVLine(196, 212, 20, SEPERATOR_COLOR);
-  gui.drawFastVLine(279, 212, 20, SEPERATOR_COLOR);
+  gui.writeFillRect(0, 202, 320, 2, BORDER_COLOR);
+  gui.writeFillRect(0, 204, 320, 36, GUI_COLOR);
+  gui.writeFastVLine(41, 212, 20, SEPERATOR_COLOR);
+  gui.writeFastVLine(196, 212, 20, SEPERATOR_COLOR);
+  gui.writeFastVLine(279, 212, 20, SEPERATOR_COLOR);
 
   canvas.createSprite(CANVAS_WIDTH, CANVAS_HEIGHT);
   canvas.fillSprite(TFT_WHITE);
 
-  M5.Display.fillScreen(BACKGROUND_COLOR);
+  drawScreen(true, true, true);
 }
 
 void loop(void) {
   M5.update();
 
-  M5.Display.waitDisplay();
-
-  if ((frames % 30) == 0) {
-    drawScreen(false, true);
-  } else {
-    drawScreen(false, false);
-  }
-
-  ++frames;
+  bool canvasMoved = false;
 
   lgfx::touch_point_t tp[1];
 
-  uint8_t num = M5.Display.getTouch(tp, 1);
+  num = M5.Display.getTouch(tp, 1);
 
-  if (num) {
-    M5.Display.startWrite();
-    M5.Display.fillCircle(tp[0].x, tp[0].y, 5, TFT_RED);
-    M5.Display.endWrite();
+  if (!pnum && num) {
+    otx = tp[0].x;
+    oty = tp[0].y;
+    oviewX = viewX;
+    oviewY = viewY;
   }
+
+  if (pnum && num) {
+    viewX = oviewX + (tp[0].x - otx);
+    viewY = oviewY + (tp[0].y - oty);
+    canvasMoved = true;
+  }
+
+  pnum = num;
+
+  if ((frames % 100) == 0) {
+    drawScreen(canvasMoved, true, canvasMoved);
+  } else {
+    drawScreen(canvasMoved, false, canvasMoved);
+  }
+
+  M5.Display.waitDisplay();
+  screen.pushSprite(0, 0);
+
+  ++frames;
 }
 
-void drawScreen(bool whole, bool clock) {
+void drawScreen(bool whole, bool drawClock, bool drawCanvas) {
   if (whole) {
-    clock = false;
+    drawClock = false;
+    drawCanvas = false;
+
+    screen.fillSprite(BACKGROUND_COLOR);
   }
   
-  M5.Display.startWrite();
+  char rbuf[32];
+  char lbuf[32];
 
-  if (whole) {
-    M5.Display.fillScreen(BACKGROUND_COLOR);
+  if (drawClock || drawCanvas || whole) {
+    m5::rtc_datetime_t dt;
+
+    if (M5.Rtc.getDateTime(&dt)) {
+      snprintf(rbuf, sizeof rbuf, "%d:%02d / %d%%", dt.time.hours, dt.time.minutes, M5.Power.getBatteryLevel());
+    } else {
+      snprintf(rbuf, sizeof rbuf, "ERROR / %d%%", M5.Power.getBatteryLevel());
+    }
+
+    snprintf(lbuf, sizeof lbuf, "(%d, %d)", viewX, viewY);
+  }
+  
+  if (drawClock) {
+    uint16_t rwidth = screen.textWidth(rbuf) + 20;
+    screen.writeFillRect(320 - 8 - rwidth, 8, rwidth, 16, BACKGROUND_COLOR);
+
+    uint16_t lwidth = screen.textWidth(lbuf) + 20;
+    screen.writeFillRect(8, 8, lwidth, 16, BACKGROUND_COLOR);
   }
 
-  if (clock) {
-    M5.Display.fillRect(0, 0, 320, 22, BACKGROUND_COLOR);
+  if (drawCanvas || ((viewY <= 24) && drawClock) || whole) {
+    canvas.pushRotateZoom(&screen, viewX + ((CANVAS_WIDTH * (uint16_t)viewZ + viewZ) >> 1), viewY + ((CANVAS_HEIGHT * (uint16_t)viewZ + viewZ) >> 1), 0, viewZ, viewZ);
+    screen.drawRect(viewX - 1, viewY - 1, CANVAS_WIDTH * viewZ + 2, CANVAS_HEIGHT * viewZ + 2, BORDER_COLOR);
+    screen.drawRoundRect(viewX - 2, viewY - 2, CANVAS_WIDTH * viewZ + 4, CANVAS_HEIGHT * viewZ + 4, 2, BORDER_COLOR);
   }
 
-  canvas.pushRotateZoom(viewX, viewY, 0, viewZ, viewZ);
+  if (drawClock || drawCanvas || whole) {
+    screen.setTextColor(BORDER_COLOR);
 
-  if (clock || whole) {
-    M5.Display.setTextDatum(top_right);
-    M5.Display.setTextColor(BORDER_COLOR);
-    M5.Display.setTextSize(1);
+    screen.setTextDatum(top_right);
+    screen.drawString(rbuf, 320 - 8, 8);
 
-    String status = "3:04 PM / " + String(M5.Power.getBatteryLevel()) + "%";
-    M5.Display.drawString(status, 320 - 8, 8);
+    screen.setTextDatum(top_left);
+    screen.drawString(lbuf, 8, 8);
   }
 
-  gui.pushSprite(0, 0, TFT_TRANSPARENT);
-
-  M5.Display.endWrite();
+  gui.pushSprite(&screen, 0, 0, TFT_TRANSPARENT);
 }
---
+++ intermediate/font/old/extract.cmd
@@ -0,0 +1,1 @@
+vfit config.json bahnschrift.ttf
